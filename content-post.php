cc6d3846db02b81b4eb2c46b159f884253227334
celinecelines <celinecelines@michaelkelly.dreamhost.com>
2012-12-29 14:31:58 -0800

    fix home page image height adjustment

--- content-post.php
+++ content-post.php
@@ -2,14 +2,11 @@
 
 $id = get_post_thumbnail_id( $post->ID );
 $image = wp_get_attachment_image_src( $id, 'home-page' );
-// var_dump( $image );
-$the_image = $image[0];
-$width = $image[1];
 $height = $image[2];
 
 ?>
 	
-	<section class="article_wrapper" style="background-image:url(<?php sld_post_thumbnail_src( $post->ID, 'home-page' ) ?>);<?php if ( $height < 600 ) : echo 'height:'. ($height+20) .'px'; endif; ?>">
+	<section class="article_wrapper" style="background-image:url(<?php sld_post_thumbnail_src( $post->ID, 'home-page' ) ?>);<?php if ( !empty($image) && $height < 600 ) : echo 'height:'. ($height+20) .'px'; endif; ?>">
 		<div>
 
 			<article>
